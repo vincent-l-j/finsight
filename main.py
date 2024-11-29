b272044ad311c9ee03bda5041765dd6b75b428ae
Vincent Jadraque <vincent.jadraque@dfcrc.com>
2024-11-29 21:44:22 +1100

    feat: show csv data

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 
 def main():
@@ -7,6 +8,10 @@ def main():
         "Upload a CSV file of transactions with columns: `date`, `description`, and `amount`."
     )
     uploaded_file = st.file_uploader("Choose a CSV file", type=["csv"])
+    if not uploaded_file:
+        return
+    df = pd.read_csv(uploaded_file)
+    st.write(df)
 
 
 if __name__ == "__main__":
